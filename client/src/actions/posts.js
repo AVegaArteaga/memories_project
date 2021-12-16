c24e62a31339b48db988ea5c8095f342c88bde3b
Abel Vega Arteaga <abelvega102@gmail.com>
2021-12-15 18:59:42 -0600

    first commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,24 @@
+import * as api from '../api';
+
+//action creators
+export const getPosts = () => async (dispatch) => {
+    
+    try {
+        const {data} = await api.fetchPost();
+
+        dispatch({type: 'FETCH_ALL', payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+  
+    
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.creatPost(post);
+        dispatch({type: 'CREATE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ server/controllers/posts.js
@@ -0,0 +1,42 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (reg, res) => {
+
+    //res.send('Cock and ball torture mr freeman, Cock and ball torture.')
+    try{
+        const postMessages = await PostMessage.find();
+        //console.log(postMessages);
+
+        res.status(200).json(postMessages);
+
+    } catch (error){
+        res.status(404).json({message: error.message});
+    }
+
+}
+
+export const createPost = async (reg, res) =>{
+
+    const post = reg.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save(); //async
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+        
+    }
+}
+
+export const updatePost = async (req, res) =>{
+    const { id: _id  } = req.params;
+    const post = req.body; //sent from the front end
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {new: true}); //reseive updated post
+
+    res.json(updatePost);
+
+}
