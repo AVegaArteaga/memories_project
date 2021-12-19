6778c27aab5534982ba8be288f78279b3c7f91a3
Abel Vega Arteaga <abelvega102@gmail.com>
2021-12-19 06:28:28 -0600

    hid passwords, dont you dare look back lol im new at this

--- client/src/styles.js
+++ client/src/styles.js
@@ -16,7 +16,7 @@ export default makeStyles((theme) => ({
     marginLeft: '15px',
   },
   [theme.breakpoints.down('sm')]:{
-    mainConatiner: {
+    mainContainer: {
       flexdirection: "colum-reverse" 
     }
   }
