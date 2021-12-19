c0fc45127f9d2dd28650d5a6898a4e8687bf3615
Abel Vega Arteaga <abelvega102@gmail.com>
2021-12-19 06:47:38 -0600

    Change image and Text to reflect more to Video Games

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/Memories1.png';
+import memories from './images/VideoGame.png';
 import useStyles from './styles';
 
 const App = () => {
@@ -23,7 +23,7 @@ const App = () => {
     return (
         <Container maxidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <Typography className={classes.heading} variant="h2" align="center">Abel's Favorite Games</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
